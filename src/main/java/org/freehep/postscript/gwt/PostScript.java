c08cbb808c68a62c69480ab5dd328cfb7a20b0f3
Mark Donszelmann <Mark.Donszelmann@cern.ch>
2010-09-30 11:45:26 +0200

    Split pom files, to be merged later

---
+++ src/main/java/org/freehep/postscript/gwt/PostScript.java
@@ -0,0 +1,16 @@
+package org.freehep.postscript.gwt;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Composite;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class PostScript extends Composite implements EntryPoint {
+
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+	}
+}
